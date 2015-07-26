1fce506da610ac0d9b4255542aedf08b7a631b03
Akshay Kumar Gupta <AkshayGupta1994@outlook.com>
2015-07-26 18:23:26 +0530

    Imporved UI 1000 times

--- MUGDApp/MUGDApp.Windows/Start.xaml.cs
+++ MUGDApp/MUGDApp.Windows/Start.xaml.cs
@@ -34,6 +34,40 @@ namespace MUGDApp
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            List<datamodel> myList = new List<datamodel>();
+            datamodel temp = new datamodel();
+            temp.back = "Red";
+            temp.name = "Register";
+            temp.src = "/Assets/Plus.png";
+            temp.title = "Add new member";
+            temp.desc = "Use this option to add new members to the group, This is similar to registeration and will not generate any user id and passwords";
+            myList.Add(temp);
+            temp = new datamodel();
+
+            temp.name = "Attendance";
+            temp.back = "Red";
+            temp.src = "/Assets/register.png";
+            temp.title = "Mark Attendance";
+            temp.desc = "Use this option to mark attendance of the members present at the event";
+            myList.Add(temp);
+            temp = new datamodel();
+
+            temp.name = "Search";
+            temp.back = "Red";
+            temp.src = "/Assets/search.png";
+            temp.title = "Search For members";
+            temp.desc = "Use this option to find members and see their details";
+            myList.Add(temp);
+            temp = new datamodel();
+
+            temp.name = "Update";
+            temp.back = "Red";
+            temp.src = "/Assets/update.png";
+            temp.title = "Update the database";
+            temp.desc = "Use this option to update members database";
+            myList.Add(temp);
+
+            Menu.DataContext = myList;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -44,6 +78,31 @@ namespace MUGDApp
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Events));
+        }
+
+        private void Menu_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            datamodel lolol = e.ClickedItem as datamodel;
+            if (lolol.title == "Register")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Delete user")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Search For users")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Add College")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Remove College")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
         }
     }
 }
--- MUGDApp/MUGDApp.WindowsPhone/Start.xaml.cs
+++ MUGDApp/MUGDApp.WindowsPhone/Start.xaml.cs
@@ -34,11 +34,68 @@ namespace MUGDApp
         /// </summary>
         /// <param name="e">Event data that describes how this page was reached.
         /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-          
-        }
 
+            protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            List<datamodel> myList = new List<datamodel>();
+            datamodel temp = new datamodel();
+            temp.back = "Red";
+            temp.name = "Register";
+            temp.src = "Pics/Event.png";
+            temp.title = "Events";
+            temp.desc = "Use this option to add new members to the group, This is similar to registeration and will not generate any user id and passwords";
+            myList.Add(temp);
+            temp = new datamodel();
+
+            temp.name = "Attendance";
+            temp.back = "Red";
+            temp.src = "/Assets/register.png";
+            temp.title = "Mark Attendance";
+            temp.desc = "Use this option to mark attendance of the members present at the event";
+            myList.Add(temp);
+            temp = new datamodel();
+
+            temp.name = "Search";
+            temp.back = "Red";
+            temp.src = "/Assets/search.png";
+            temp.title = "Search For members";
+            temp.desc = "Use this option to find members and see their details";
+            myList.Add(temp);
+            temp = new datamodel();
+
+            temp.name = "Update";
+            temp.back = "Red";
+            temp.src = "/Assets/update.png";
+            temp.title = "Update the database";
+            temp.desc = "Use this option to update members database";
+            myList.Add(temp);
+
+            Menu.DataContext = myList;
+        }
+         private void Menu_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            datamodel lolol = e.ClickedItem as datamodel;
+            if (lolol.title == "Register")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Delete user")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Search For users")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Add College")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+            else if (lolol.title == "Remove College")
+            {
+                Frame.Navigate(typeof(MainPage));
+            }
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //this.Frame.Navigate(typeof(Login));
