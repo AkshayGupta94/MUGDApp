06b1c1e7e64921397bc7466068209c08336b769b
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-02 13:14:48 -0700

    last for tonight

--- MUGDApp/MUGDApp.WindowsPhone/MainPage.xaml.cs
+++ MUGDApp/MUGDApp.WindowsPhone/MainPage.xaml.cs
@@ -63,7 +63,19 @@ namespace MUGDApp
 
         async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-           
+            PushNotificationChannel channel;
+            channel = await Windows.Networking.PushNotifications.PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+            try
+            {
+                await App.Mugd_appClient.GetPush().RegisterNativeAsync(channel.Uri);
+                //await App.Mugd_appClient.InvokeApiAsync("notifyAllUsers",
+                //    new JObject(new JProperty("toast", "Sample Toast")));
+            }
+            catch (Exception exception)
+            {
+                HandleRegisterException(exception);
+            }
+            channel.PushNotificationReceived += channel_PushNotificationReceived;
             test = new ObservableCollection<ChatPubList>();
             
             items = await Table.OrderByDescending(ChatPublic=> ChatPublic.CreatedAt ).ToCollectionAsync();
@@ -89,6 +101,31 @@ namespace MUGDApp
             }
             lol.ItemsSource = test;
             test.CollectionChanged += test_CollectionChanged;
+        }
+        async void channel_PushNotificationReceived(Windows.Networking.PushNotifications.PushNotificationChannel sender, Windows.Networking.PushNotifications.PushNotificationReceivedEventArgs args)
+        {
+            if (args.ToastNotification.Content.InnerText.Contains("Event"))
+            {
+                //args.Cancel = true;
+
+            }
+            else
+            {
+                args.Cancel = true;
+                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    ChatPublic c = new ChatPublic();
+                    ChatPubList a = new ChatPubList();
+                    a.Message = args.ToastNotification.Content.InnerText;
+                    // c.Name = args.ToastNotification.Content.InnerText.Substring(index + 4, args.ToastNotification.Content.InnerText.Length - index + 5);
+                    MainPage.test.Insert(0, a);
+
+
+                });
+
+
+
+            }
         }
         private static void HandleRegisterException(Exception exception)
         {
