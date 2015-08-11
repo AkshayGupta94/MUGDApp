481c6fef35a0289299ee91a8fff608b66094f7a7
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-11 23:10:23 +0530

    Voting logic added! Now a person can vote for one idea only once!

--- MUGDApp/MUGDApp.WindowsPhone/UserVoice.xaml.cs
+++ MUGDApp/MUGDApp.WindowsPhone/UserVoice.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,6 +25,7 @@ namespace MUGDApp
     /// </summary>
     public sealed partial class UserVoice : Page
     {
+        private List<string> ids;
         private IMobileServiceTable<idea> Table = App.MobileService.GetTable<idea>();
         private MobileServiceCollection<idea, idea> items;
         public UserVoice()
@@ -37,39 +40,146 @@ namespace MUGDApp
         /// This parameter is typically used to configure the page.</param>
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-            items = await Table.ToCollectionAsync();
-            //List<idea> ai = new List<idea>();
-            //idea i = new idea();
-            //i.Title = "Test";
-            //i.option1 = "Hello1";
-            //i.option2 = "Hello2";
-            //i.option3 = "Hello3";
-            //ai.Add(i);
-            Voice.ItemsSource = items;
+            StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile =
+                await folder.CreateFileAsync("Sample.txt", CreationCollisionOption.OpenIfExists);
+            //await Windows.Storage.FileIO.WriteTextAsync(sampleFile, );
+            try
+            {
+                string testlol = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+                ids = JsonConvert.DeserializeObject<List<string>>(testlol);
+            }
+            finally
+            {
+
+
+                items = await Table.ToCollectionAsync();
+                //List<idea> ai = new List<idea>();
+                //idea i = new idea();
+                //i.Title = "Test";
+                //i.option1 = "Hello1";
+                //i.option2 = "Hello2";
+                //i.option3 = "Hello3";
+                //ai.Add(i);
+                Voice.ItemsSource = items;
+            }
         }
 
         private async void Option1_Click(object sender, RoutedEventArgs e)
         {
             Button temp = sender as Button;
             idea i = temp.DataContext as idea;
-            i.Countoption1++;
-            await Table.UpdateAsync(i);
+            if (ids != null)
+            {
+                if (ids.Contains(i.Id))
+                {
+                    Windows.UI.Popups.MessageDialog mess = new Windows.UI.Popups.MessageDialog("You have already voted for this idea");
+                    await mess.ShowAsync();
+                }
+                else
+                {
+                    ids.Add(i.Id);
+                    string test = JsonConvert.SerializeObject(ids);
+                    StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                    StorageFile sampleFile =
+                        await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+
+                    await Windows.Storage.FileIO.WriteTextAsync(sampleFile, test);
+                    i.Countoption1++;
+                    await Table.UpdateAsync(i);
+                }
+            }
+            else
+            {
+                ids = new List<string>();
+                ids.Add(i.Id);
+                string test = JsonConvert.SerializeObject(ids);
+                StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                StorageFile sampleFile =
+                    await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+
+                await Windows.Storage.FileIO.WriteTextAsync(sampleFile, test);
+                i.Countoption1++;
+                await Table.UpdateAsync(i);
+            }
         }
 
         private async void Option2_Click(object sender, RoutedEventArgs e)
         {
             Button temp = sender as Button;
             idea i = temp.DataContext as idea;
-            i.Countoption2++;
-            await Table.UpdateAsync(i);
+            if (ids != null)
+            {
+                if (ids.Contains(i.Id))
+                {
+                    Windows.UI.Popups.MessageDialog mess = new Windows.UI.Popups.MessageDialog("You have already voted for this idea");
+                    await mess.ShowAsync();
+                }
+                else
+                {
+                    ids.Add(i.Id);
+                    string test = JsonConvert.SerializeObject(ids);
+                    StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                    StorageFile sampleFile =
+                        await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+
+                    await Windows.Storage.FileIO.WriteTextAsync(sampleFile, test);
+                    i.Countoption1++;
+                    await Table.UpdateAsync(i);
+                }
+            }
+            else
+            {
+                ids = new List<string>();
+                ids.Add(i.Id);
+                string test = JsonConvert.SerializeObject(ids);
+                StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                StorageFile sampleFile =
+                    await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+
+                await Windows.Storage.FileIO.WriteTextAsync(sampleFile, test);
+                i.Countoption2++;
+                await Table.UpdateAsync(i);
+            }
         }
 
         private async void Option3_Click(object sender, RoutedEventArgs e)
         {
             Button temp = sender as Button;
             idea i = temp.DataContext as idea;
-            i.Countoption3++;
-            await Table.UpdateAsync(i);
+            if (ids != null)
+            {
+                if (ids.Contains(i.Id))
+                {
+                    Windows.UI.Popups.MessageDialog mess = new Windows.UI.Popups.MessageDialog("You have already voted for this idea");
+                    await mess.ShowAsync();
+                }
+                else
+                {
+                    ids.Add(i.Id);
+                    string test = JsonConvert.SerializeObject(ids);
+                    StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                    StorageFile sampleFile =
+                        await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+
+                    await Windows.Storage.FileIO.WriteTextAsync(sampleFile, test);
+                    i.Countoption1++;
+                    await Table.UpdateAsync(i);
+                }
+            }
+            else
+            {
+                ids = new List<string>();
+                ids.Add(i.Id);
+                string test = JsonConvert.SerializeObject(ids);
+                StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                StorageFile sampleFile =
+                    await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+
+                await Windows.Storage.FileIO.WriteTextAsync(sampleFile, test);
+                i.Countoption3++;
+                await Table.UpdateAsync(i);
+            }
         }
     }
 }
